f004796a58bdbc4c176e9590bcf8c9dd26fae91f
Juanpablo02 <71158227+Juanpablo02@users.noreply.github.com>
2022-08-24 22:11:28 -0500

    NTJRO2 - Se realizo el reto 2 y 3 con bucles y condicionales

--- retos2.py
+++ retos2.py
@@ -8,7 +8,7 @@ def reto1():
     else: 
         print(f'Se digito un numero negativo {numero}, el programa de cerró')
     
-reto1()
+#reto1()
 
 
 def reto2():
@@ -20,40 +20,111 @@ def reto2():
         print("**********************")
         print("")
         print("Digite porfavor el digito de la opcíon que desees ejecutar")
+        print("")
         print("0 : Salida")
         print("1 : Encuentre si el número es múltiplo de 2")
         print("2 : Encuentre la raíz cuadrada del número")
         print("3 : Sume 100 al número ingresado")
         print("4 : Eleve a la 2 el número ingresado")
+        print("")
         selector = int(input("Seleccionar: "))
         
         if selector == 1:
-            print("**** Bienvenido aquí podras encontrar que número es múltiplo de 2 ****")
-            numero = int(input("Porfavor digite un número: "))
-            if numero % 2 == 0:
-                print(f'El numero {numero}, si es multiplo de 2')
-            else:
-                print(f'El numero {numero}, no es multiplo de 2')
+            contador1 = 1
+            while contador1 != 0:
+                print("**** Bienvenido aquí podras encontrar que número es múltiplo de 2 ****")
+                print("")
+                numero = int(input("Porfavor digite un número: "))
+                print("")
+                if numero % 2 == 0:
+                    print(f'-> El numero {numero}, si es multiplo de 2')
+                    print("")
+                else:
+                    print(f'-> El numero {numero}, no es multiplo de 2')
+                    print("")
+                print("*** Deseas seguir digitando números o deseas salir al menú? ***")
+                print("")
+                print("0 : Si deseas salir al menú")
+                print("1 : Si deseas seguir digitando numeros")
+                print("")
+                contador1 = int(input("Selecciona: "))
+                if contador1 == 0:
+                    contador1 = 0
+                    
         elif selector == 2:
-            print("**** Bienvenido aquí podras encontrar la raiz cuadrada de cualquier número ****")
-            numero = int(input("Porfavor digite un número: "))
-            resultado = math.sqrt(numero)
-            print(f'La raiz cuadrada de {numero} es {resultado}')            
+            contador1 = 1
+            while contador1 != 0:
+                print("**** Bienvenido aquí podras encontrar la raiz cuadrada de cualquier número ****")
+                print("")
+                numero = int(input("Porfavor digite un número: "))
+                print("")
+                resultado = math.sqrt(numero)
+                print(f'-> La raiz cuadrada de {numero} es {resultado}')  
+                print("")
+                print("*** Deseas seguir digitando números o deseas salir al menú? ***")
+                print("")
+                print("0 : Si deseas salir al menú")
+                print("1 : Si deseas seguir digitando numeros")
+                print("")
+                contador1 = int(input("Selecciona: "))
+                if contador1 == 0:
+                    contador1 = 0          
         elif selector == 3:
-            print("**** Bienvenido aquí podras sumar 100 a cualquier número ****")
-            numero = int(input("Porfavor digite un número: "))
-            resultado = numero + 100
-            print(f'El número que ingresaste es {numero} y sumandole 100 es {resultado}')
+            contador1 = 1
+            while contador1 != 0:
+                print("**** Bienvenido aquí podras sumar 100 a cualquier número ****")
+                print("")
+                numero = int(input("Porfavor digite un número: "))
+                print("")
+                resultado = numero + 100
+                print(f'-> El número que ingresaste es {numero} y sumandole 100 es {resultado}')
+                print("")
+                print("*** Deseas seguir digitando números o deseas salir al menú? ***")
+                print("")
+                print("0 : Si deseas salir al menú")
+                print("1 : Si deseas seguir digitando numeros")
+                print("")
+                contador1 = int(input("Selecciona: "))
+                if contador1 == 0:
+                    contador1 = 0      
         elif selector == 4:
-            print("**** Bienvenido aquí podras elevar a la 2 cualquier número ****")
-            numero = int(input("Porfavor digite un número: "))
-            resultado = numero ** 2
-            print(f'El resultado del número {numero} elevando a la dos es {resultado}')
+            contador1 = 1
+            while contador1 != 0:
+                print("**** Bienvenido aquí podras elevar a la 2 cualquier número ****")
+                print("")
+                numero = int(input("Porfavor digite un número: "))
+                print("")
+                resultado = numero ** 2
+                print(f'-> El resultado del número {numero} elevando a la dos es {resultado}')
+                print("")
+                print("*** Deseas seguir digitando números o deseas salir al menú? ***")
+                print("")
+                print("0 : Si deseas salir al menú")
+                print("1 : Si deseas seguir digitando numeros")
+                print("")
+                contador1 = int(input("Selecciona: "))
+                if contador1 == 0:
+                    contador1 = 0      
         elif selector == 0:
+            print("")
             print("**** Escogiste salir del programa, muchas gracias por visitarnos ****")
             contador = 0
         else:
             print("Porfavor digita un número valido")
 
-reto2()
+#reto2()
+
+def reto3():
+    contador = 1
+    numero = 1
+    print(numero)
+    numero = numero + 11
+    print(numero)
+    while contador != 0:
+        numero = numero + 12
+        print(numero)
+        if numero >= 200:
+            contador = 0
+            
+reto3()
 
